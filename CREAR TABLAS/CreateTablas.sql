7ecdeb070f6e46500815b4f9140897626434eb61
rodrigo sebastian vega centeno <75128404+rodrigo1857@users.noreply.github.com>
2025-01-10 16:19:29 -0500

    Update CreateTablas.sql

--- CREAR TABLAS/CreateTablas.sql
+++ CREAR TABLAS/CreateTablas.sql
@@ -1,4 +1,4 @@
--====== creacion de tablas ==========
+--====== creacion de tablas ==========
 ---------------------1---------------------
 
 create table bytsscom_bytcore.area_patrimonio_dependencias
@@ -29,3 +29,67 @@ create table bytsscom_bytcore.area_patrimonio_unidades
 
 alter table bytsscom_bytcore.area_patrimonio_unidades
     owner to postgres;
+------------------NORMALIZACION DE TABLAS PARA PATRIMONIO ------------------------
+
+
+
+CREATE TABLE "registro" (
+    "idregistro"  SERIAL  NOT NULL,
+    "fech_registro" TIMESTAMP   NOT NULL,
+    "fech_actualizacion" TIMESTAMP   NOT NULL,
+    "id_ambiente" INT   NOT NULL,
+    -- idpersona
+    "id_asignado" INT   NOT NULL,
+    "num_pecosa" STRING   NOT NULL,
+    "guia_remision" STRING   NOT NULL,
+    "boleta" STRING   NOT NULL,
+    "detalle" TEXT   NOT NULL,
+    CONSTRAINT "pk_registro" PRIMARY KEY (
+        "idregistro"
+     )
+);
+
+CREATE TABLE "bienes" (
+    "idbien"  SERIAL  NOT NULL,
+    "idregistro" INT   NOT NULL,
+    "iditem" INT   NOT NULL,
+    "correlativo" INT   NOT NULL,
+    "marca" TEXT   NOT NULL,
+    "model" TEXT   NOT NULL,
+    "serie" TEXT   NOT NULL,
+    "dimension" TEXT   NOT NULL,
+    "color" TEXT   NOT NULL,
+    "precio" FLOAT   NOT NULL,
+    "tipo_bien" STRING   NOT NULL,
+    CONSTRAINT "pk_bienes" PRIMARY KEY (
+        "idbien"
+     )
+);
+
+CREATE TABLE "registroOC" (
+    "idregistroOC"  SERIAL  NOT NULL,
+    "idorden" INT   NOT NULL,
+    "idregistro" INT   NOT NULL,
+    CONSTRAINT "pk_registroOC" PRIMARY KEY (
+        "idregistroOC"
+     )
+);
+
+CREATE TABLE "registroNEA" (
+    "idregistroNEA"  SERIAL  NOT NULL,
+    "idregistro" INT   NOT NULL,
+    "id_proveedor" INT   NOT NULL,
+    "id_proyecto" INT   NOT NULL,
+    CONSTRAINT "pk_registroNEA" PRIMARY KEY (
+        "idregistroNEA"
+     )
+);
+
+ALTER TABLE "bienes" ADD CONSTRAINT "fk_bienes_idregistro" FOREIGN KEY("idregistro")
+REFERENCES "registro" ("idregistro");
+
+ALTER TABLE "registroOC" ADD CONSTRAINT "fk_registroOC_idregistro" FOREIGN KEY("idregistro")
+REFERENCES "registro" ("idregistro");
+
+ALTER TABLE "registroNEA" ADD CONSTRAINT "fk_registroNEA_idregistro" FOREIGN KEY("idregistro")
+REFERENCES "registro" ("idregistro");
