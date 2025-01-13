56ca8973c29b2e3a333e570c65c11c23fd1a02bc
Kevin Ramos Rivas <80496624+kevinramosrivas@users.noreply.github.com>
2025-01-13 11:44:52 -0500

    Update CreateTablas.sql

--- CREAR TABLAS/CreateTablas.sql
+++ CREAR TABLAS/CreateTablas.sql
@@ -1,95 +1,127 @@
---====== creacion de tablas ==========
----------------------1---------------------
+-- TABLA PATRIMONIO AREA
+------------------------------------------------
 
-create table bytsscom_bytcore.area_patrimonio_dependencias
+create table if not exists public.patrimonio_area
 (
-    id_area_patrimonio integer      not null
-        primary key,
-    codigo             varchar(100)  not null,
-    nombre             varchar      not null,
-    tipo               varchar(100) not null
+    id_patrimonio_area integer not null
+        constraint pk_patrimonio_area
+            primary key
+        constraint patrimonio_area_area_id_area_fk
+            references bytsscom_bytcore.area,
+    codigo             varchar not null,
+    nombre             varchar not null,
+    tipo               varchar not null
 );
 
-comment on column bytsscom_bytcore.area_patrimonio_dependencias.id_area_patrimonio is 'TIPO : Ambiente,Dependencia,Oficina,Unidad,SubUnidad';
-
-alter table bytsscom_bytcore.area_patrimonio_dependencias
+alter table public.patrimonio_area
     owner to postgres;
 
----------------------2---------------------
-create table bytsscom_bytcore.area_patrimonio_unidades
+-- TABLA PATRIMONIO AMBIENTE
+------------------------------------------------
+create table if not exists public.patrimonio_ambiente
 (
-    id_patrimonio_unidad serial
-        primary key,
-    id_area_patrimonio   integer
-        references bytsscom_bytcore.area_patrimonio_dependencias,
-    codigo               varchar(100) not null,
-    nombre               varchar      not null,
-    tipo                 varchar(100) not null
+    id_patrimonio_ambiente serial
+        constraint pk_patrimonio_ambiente
+            primary key,
+    id_patrimonio_area     integer not null
+        constraint fk_patrimonio_ambiente_id_patrimonio_area
+            references public.patrimonio_area,
+    codigo                 varchar not null,
+    nombre                 varchar not null,
+    tipo                   varchar not null
 );
 
-alter table bytsscom_bytcore.area_patrimonio_unidades
+alter table public.patrimonio_ambiente
     owner to postgres;
-------------------NORMALIZACION DE TABLAS PARA PATRIMONIO ------------------------
 
-
-
-CREATE TABLE "registro" (
-    "idregistro"  SERIAL  NOT NULL,
-    "fech_registro" TIMESTAMP   NOT NULL,
-    "fech_actualizacion" TIMESTAMP   NOT NULL,
-    "id_ambiente" INT   NOT NULL,
-    -- idpersona
-    "id_asignado" INT   NOT NULL,
-    "num_pecosa" STRING   NOT NULL,
-    "guia_remision" STRING   NOT NULL,
-    "boleta" STRING   NOT NULL,
-    "detalle" TEXT   NOT NULL,
-    CONSTRAINT "pk_registro" PRIMARY KEY (
-        "idregistro"
-     )
+-- TABLA PATRIMONIO REGISTRO
+------------------------------------------------
+create table if not exists public.patrimonio_registro
+(
+    id_patrimonio_registro serial
+        constraint pk_patrimonio_registro
+            primary key,
+    fech_registro          timestamp not null,
+    fech_actualizacion     timestamp not null,
+    id_patrimonio_ambiente integer   not null
+        constraint fk_patrimonio_registro_id_patrimonio_ambiente
+            references public.patrimonio_ambiente,
+    id_persona_asignada    integer   not null
+        constraint patrimonio_registro_persona_id_persona_fk
+            references bytsscom_bytcore.persona,
+    num_factura            varchar,
+    num_boleta             varchar,
+    num_guia_remision      varchar,
+    num_pecosa             varchar   not null,
+    detalle                text
 );
 
-CREATE TABLE "bienes" (
-    "idbien"  SERIAL  NOT NULL,
-    "idregistro" INT   NOT NULL,
-    "iditem" INT   NOT NULL,
-    "correlativo" INT   NOT NULL,
-    "marca" TEXT   NOT NULL,
-    "model" TEXT   NOT NULL,
-    "serie" TEXT   NOT NULL,
-    "dimension" TEXT   NOT NULL,
-    "color" TEXT   NOT NULL,
-    "precio" FLOAT   NOT NULL,
-    "tipo_bien" STRING   NOT NULL,
-    CONSTRAINT "pk_bienes" PRIMARY KEY (
-        "idbien"
-     )
+alter table public.patrimonio_registro
+    owner to postgres;
+
+
+-- TABLA PATRIMONIO REGISTRO ORDEN DE COMPRA
+------------------------------------------------
+create table if not exists public.patrimonio_regoc
+(
+    id_patrimonio_regoc    serial
+        constraint pk_patrimonio_regoc
+            primary key,
+    id_patrimonio_registro integer not null
+        constraint fk_patrimonio_regoc_id_patrimonio_registro
+            references public.patrimonio_registro,
+    id_contrato            integer not null
+        constraint patrimonio_regoc_contrato_id_contrato_fk
+            references bytsscom_bytsig.contrato
 );
 
-CREATE TABLE "registroOC" (
-    "idregistroOC"  SERIAL  NOT NULL,
-    "idorden" INT   NOT NULL,
-    "idregistro" INT   NOT NULL,
-    CONSTRAINT "pk_registroOC" PRIMARY KEY (
-        "idregistroOC"
-     )
+alter table public.patrimonio_regoc
+    owner to postgres;
+
+
+-- TABLA PATRIMONIO REGISTRO NEA
+------------------------------------------------
+
+create table if not exists public.patrimonio_regnea
+(
+    id_patrimonio_regnea   serial
+        constraint pk_patrimonio_regnea
+            primary key,
+    id_patrimonio_registro integer not null
+        constraint fk_patrimonio_regnea_id_patrimonio_registro
+            references public.patrimonio_registro,
+    id_proveedor           integer not null
+        constraint patrimonio_regnea_persona_id_persona_fk
+            references bytsscom_bytcore.persona,
+    id_proyecto            integer
 );
 
-CREATE TABLE "registroNEA" (
-    "idregistroNEA"  SERIAL  NOT NULL,
-    "idregistro" INT   NOT NULL,
-    "id_proveedor" INT   NOT NULL,
-    "id_proyecto" INT   NOT NULL,
-    CONSTRAINT "pk_registroNEA" PRIMARY KEY (
-        "idregistroNEA"
-     )
+alter table public.patrimonio_regnea
+    owner to postgres;
+
+-- TABLA PATRIMONIO BIEN
+------------------------------------------------
+create table if not exists public.patrimonio_bien
+(
+    id_patrimonio_bien     serial
+        constraint pk_patrimonio_bien
+            primary key,
+    id_patrimonio_registro integer          not null
+        constraint fk_patrimonio_bien_id_patrimonio_registro
+            references public.patrimonio_registro,
+    id_item                integer          not null
+        constraint patrimonio_bien_item_id_item_fk
+            references bytsscom_bytsig.item,
+    correlativo            varchar          not null,
+    marca                  varchar          not null,
+    modelo                 varchar          not null,
+    serie                  varchar,
+    dimension              varchar,
+    color                  varchar,
+    precio                 double precision not null,
+    tipo_bien              varchar          not null
 );
 
-ALTER TABLE "bienes" ADD CONSTRAINT "fk_bienes_idregistro" FOREIGN KEY("idregistro")
-REFERENCES "registro" ("idregistro");
+alter table public.patrimonio_bien
+    owner to postgres;
 
-ALTER TABLE "registroOC" ADD CONSTRAINT "fk_registroOC_idregistro" FOREIGN KEY("idregistro")
-REFERENCES "registro" ("idregistro");
-
-ALTER TABLE "registroNEA" ADD CONSTRAINT "fk_registroNEA_idregistro" FOREIGN KEY("idregistro")
-REFERENCES "registro" ("idregistro");
